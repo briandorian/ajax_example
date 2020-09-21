da3853a58531b74fd182220420a2f5cfc3ac2c1b
Brian Acosta <brian.acosta@xing.com>
2020-09-21 21:13:55 +0200

    modifications to have several doggos shown , no styling yet

--- doggos.js
+++ doggos.js
@@ -1,18 +1,21 @@
 const BREEDS_URL = "https://dog.ceo/api/breeds/image/random"
 const doggos = document.querySelector(".doggos");
 
+
+function addNewDogo(){
 const promise = fetch(BREEDS_URL);
+
 promise
     .then(function(response){
-        const processingPromise=response.json();
+        let processingPromise=response.json();
         return processingPromise;
     })
     .then(function (processedReponse) {
-        const img=document.createElement("img");
+        let img=document.createElement("img");
         img.src=processedReponse.message;
         img.alt="Cute doggo"
         doggos.appendChild(img);
     });
-
-console.log("This will log first");
+}
+document.querySelector(".addDogo").addEventListener("click",addNewDogo);
 
